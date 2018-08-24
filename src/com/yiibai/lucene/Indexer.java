86e1fe1f95319855403e9ce7d6f2912744d54a5d
Leecole <1903502727@qq.com>
2018-08-24 11:19:35 +0800

    Lucene

--- src/com/yiibai/lucene/Indexer.java
+++ src/com/yiibai/lucene/Indexer.java
@@ -28,10 +28,10 @@ public class Indexer {
         @Override
         public boolean accept(File pathname) {
 // TODO Auto-generated method stub
-            return pathname.getName().toLowerCase().endsWith(".txt");
+            return pathname.getName().toLowerCase().endsWith(".txt");   //文档一（.txt）文件结束
         }
-
     }
+
     //构造方法，用来传入索引存放路径
     public Indexer(String indexDir) throws IOException{
         Directory dir=FSDirectory.open(Paths.get(indexDir));
@@ -39,10 +39,12 @@ public class Indexer {
         config.setOpenMode(IndexWriterConfig.OpenMode.CREATE_OR_APPEND);
         writer=new IndexWriter(dir,config);
     }
+
     //关闭indexWriter
     public void close() throws IOException{
         writer.close();
     }
+
     //这个方法是遍历文件夹下所有文件并选择符合条件文件写入索引的方法，返回写入的文档总数
     public int index(String dataDir,FileFilter filter) throws IOException{
         File[] files=new File(dataDir).listFiles();
@@ -56,12 +58,14 @@ public class Indexer {
         }
         return writer.numDocs();
     }
+
     //这个方法是写入索引的方法，将生成的document对象写入到索引中
     private void indexFile(File file) throws IOException{
         System.out.println("indexing..."+file.getCanonicalPath());
         Document doc=getDocument(file);
         writer.addDocument(doc);
     }
+
     //这个方法是生成Document对象的方法，Document对象就是对文档各个属性的封装
     protected Document getDocument(File file) throws IOException{
         Document doc=new Document();
@@ -72,17 +76,17 @@ public class Indexer {
     }
 
     public static void main(String[] args) throws IOException {
-        String indexDir="indexer";
-        String dataDir="file";
+        String indexDir="indexer";  //存放索引路径
+        String dataDir="file";  //存放数据文件路径
 
         long start=System.currentTimeMillis();
         Indexer indexer=new Indexer(indexDir);
 
         int numberIndexed=indexer.index(dataDir, new TextFilesFilter());
 
-        indexer.close();
+        indexer.close();    //必须要关闭indexer
         long end=System.currentTimeMillis();
-        System.out.println(numberIndexed);
-        System.out.println(end-start);
+        System.out.println("共找到"+numberIndexed+"条数据");
+        System.out.println("用时"+ (end-start)+"毫秒");
     }
 }
